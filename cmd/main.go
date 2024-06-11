6a7b090b5394fcf5c3dd867525506f78ca40e6c5
Yusuf Cemal Celebi <yusuf.celebi@hazelcast.com>
2024-06-11 17:47:55 +0300

    EmailSenderConfig API template is added

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	examplev1 "github.com/yusufcemalcelebi/kubernetes-operator-example/api/v1"
+	"github.com/yusufcemalcelebi/kubernetes-operator-example/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -44,6 +47,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(examplev1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -119,6 +123,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.EmailSenderConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "EmailSenderConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
